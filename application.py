92485faf1858a67ab4464be9fce2e59dd0672b11
m-gouveia <32441957+m-gouveia@users.noreply.github.com>
2021-03-24 17:39:13 -0300

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hola FIAP! (Ambiente de produção) </h1>\nMBA! v3 o/"
+    return "<h1>Hola FIAP! (Ambiente de produção) </h1>\nMBA! v4 o/"
